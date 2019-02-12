a89c6acd389bdbc9a3b50af86c462bd82fb625ec
Eric Yang <ery006@ucsd.edu>
2019-02-12 12:16:06 -0800

    fixed map errors

--- public/js/map.js
+++ public/js/map.js
@@ -42,32 +42,6 @@ $('.dropdown-menu.keep-open').on('click', function (e) {
 function initMap() {
 	// Create an array of alphabetical characters used to label the markers.
 	var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-<<<<<<< HEAD
-
-	// var allLocations
-	// for(var loc in locs.locations) {
-	// 	console.log(loc);
-	// }
-
-	// // Add some markers to the map.
-	// // Note: The code uses the JavaScript Array.prototype.map() method to
-	// // create an array of markers based on a given "locations" array.
-	// // The map() method here has nothing to do with the Google Maps API.
-	// var markers = locations.map(function(location, i) {
-	//   return new google.maps.Marker({
-	//     position: location,
-	//     label: labels[i % labels.length]
-	//   });
-	// });
-
-	var image = "https://img.icons8.com/color/26/000000/filled-star.png";
-	var currrentLoc = new google.maps.Marker({
-          position: latlng,
-          map: map,
-					icon: image
-    });
-=======
->>>>>>> 09d1190cb44cf2567c4bd4a3826996ed995531fe
 
 	var currentPosition = new google.maps.LatLng(32.877281, -117.234929);
 
@@ -310,88 +284,5 @@ function studyGroupsMap() {
 	 //      infowindow.open(map, marker);
 	 //    });
 
-<<<<<<< HEAD
-	// Create an array of alphabetical characters used to label the markers.
-	var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-	var image = "https://img.icons8.com/color/26/000000/filled-star.png";
-	var currrentLoc = new google.maps.Marker({
-					position: latlng,
-					map: map,
-					icon: image
-		});
-
-	var marker1 = new google.maps.Marker({
-          position: loc1,
-          map: map,
-          label: 'A'
-    });
-
-    var marker2 = new google.maps.Marker({
-          position: loc2,
-          map: map,
-          label: 'B'
-    });
-
-    var marker3 = new google.maps.Marker({
-          position: loc3,
-          map: map,
-          label: 'C'
-    });
-
 }
-
-function studyGroupsMap() {
-	var latlng = new google.maps.LatLng(32.877281, -113.234929);
-	var loc1 = new google.maps.LatLng(32.876616, -114.231710);
-	var loc2 = new google.maps.LatLng(32.868518, -114.239889);
-	var loc3 = new google.maps.LatLng(32.870865, -113.211506);
-	var map = new google.maps.Map(document.getElementById('map'),  {
-	    zoom: 12,
-	    center: latlng,
-	});
-
-	// Create an array of alphabetical characters used to label the markers.
-	var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-	// // Add some markers to the map.
-	// // Note: The code uses the JavaScript Array.prototype.map() method to
-	// // create an array of markers based on a given "locations" array.
-	// // The map() method here has nothing to do with the Google Maps API.
-	// var markers = locations.map(function(location, i) {
-	//   return new google.maps.Marker({
-	//     position: location,
-	//     label: labels[i % labels.length]
-	//   });
-	// });
-
-	var image = "https://img.icons8.com/color/26/000000/filled-star.png";
-	var currrentLoc = new google.maps.Marker({
-					position: latlng,
-					map: map,
-					icon: image
-		});
-
-	var marker1 = new google.maps.Marker({
-          position: loc1,
-          map: map,
-          label: 'A'
-    });
-
-    var marker2 = new google.maps.Marker({
-          position: loc2,
-          map: map,
-          label: 'B'
-    });
-
-    var marker3 = new google.maps.Marker({
-          position: loc3,
-          map: map,
-          label: 'C'
-    });
-=======
-	    markers.push(marker);
->>>>>>> 09d1190cb44cf2567c4bd4a3826996ed995531fe
-
-	}
 }
