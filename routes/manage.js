faedabe65d652437be2e7d10e4aca543766ef182
Angela Zhu <anz047@ucsd.edu>
2019-02-21 21:18:02 -0800

    new

--- routes/manage.js
+++ routes/manage.js
@@ -7,5 +7,9 @@ var data = require('../favorites.json');
 
 exports.view = function(req, res){
   console.log(data.locs);
+  console.log(data.result);
+  console.log("------------ deleting -------------");
+  delete data.result[1];
+  console.log(data.result);
   res.render('manage', data);
 };
